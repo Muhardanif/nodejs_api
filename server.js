8fcf6f2827faeef7cf1c5b9f13f17c21262ee759
unknown <danifwahyudi@gmail.com>
2022-09-17 09:07:14 +0700

    change BAS

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 const admin = require("firebase-admin");
-const credentials = require("./serviceAccountKey.json");
+const credentials = require("./serviceAccountKeyBAS.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(credentials)
@@ -31,7 +31,8 @@ app.post('/create', async(req, res) => {
 
 app.get('/read/all', async(req, res) => {
     try{
-        const usersRef = db.collection("users");
+        const usersRef = db.collection("CustomerData");
+        // const usersRef = db.collection("users");
         const response = await usersRef.get();
         let responseArr = [];
         response.forEach(doc => {
